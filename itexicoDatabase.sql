51e15cc3056dfe5b6c3d3ae06b0fccbe2cbade8d
oscarhdezs <36929495+oscarhdezs@users.noreply.github.com>
2019-01-06 17:36:13 -0600

    Update and rename Changes to itexicoDatabase.sql

---
+++ itexicoDatabase.sql
@@ -0,0 +1,37 @@
+Script database
+use itexico;
+
+DROP TABLE IF EXISTS user;
+CREATE TABLE user (
+  user_id  INT(11) NOT NULL  UNIQUE AUTO_INCREMENT,
+  first_name VARCHAR(100)  NOT NULL UNIQUE,
+  last_name VARCHAR(100)  NOT NULL,
+  creation_date  DATE NOT NULL,
+  active BOOLEAN NOT NULL,
+  PRIMARY KEY(user_id)
+  );
+  
+ DROP TABLE IF EXISTS role;
+  CREATE TABLE role(
+  role_id INT(11) NOT NULL  AUTO_INCREMENT,
+  role_name VARCHAR(20) UNIQUE NOT NULL,
+  active BOOLEAN NOT NULL,
+  PRIMARY KEY (role_id)
+  );
+
+  
+  DROP TABLE IF EXISTS user_roles;
+  CREATE TABLE user_roles(
+  user_id INT(11),
+  role_id INT(11),
+  PRIMARY KEY(user_id,role_id),
+  CONSTRAINT FK_USER
+  FOREIGN KEY (user_id)
+  REFERENCES user(user_id)
+  ON DELETE NO ACTION ON UPDATE NO ACTION, 
+  
+  CONSTRAINT FK_ROLE
+  FOREIGN KEY (role_id)
+  REFERENCES role(role_id)
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+  );
